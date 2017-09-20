f21a2e06d3d2bb4e7b10523dcfc734e024607c0d
Natasha Meinzen <31399883+natashameinzen@users.noreply.github.com>
2017-09-19 21:33:57 -1000

    sums up numbers

---
+++ 5under5.js
@@ -0,0 +1,9 @@
+function sumofnumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+const testList = [1, 2, 3, 4, 5];
+console.log(sumofnumbers(testList));
